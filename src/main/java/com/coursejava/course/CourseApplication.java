3c60546cfe45d724e0f8e174de31510a14530217
LucasGarciaG <lucaslggf.lg@gmail.com>
2022-05-10 14:01:23 -0300

    Projeto criado

---
+++ src/main/java/com/coursejava/course/CourseApplication.java
@@ -0,0 +1,14 @@
+package com.coursejava.course;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseApplication {
+
+	public static void main(String[] args) {
+		//Primeiro comando para o spring boot rodar.
+		SpringApplication.run(CourseApplication.class, args);
+	}
+
+}
